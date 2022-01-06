1c1018d6a5b82b1c45017609efa364497b4e4115
Laith-Grira <lgrira12@gmail.com>
2022-01-06 17:58:50 -0500

    adding styles to the app

--- src/components/NasaPicture.js
+++ src/components/NasaPicture.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
+import './NasaPicture.scss';
 
 const NasaPicture = props => {
 
     const [pictureLiked, setPictureLiked] = useState(false);
 
     return ( 
-        <div className="element-div">
+        <div className="element-modal">
             <h2 id="picture-title">{props.title}</h2>
             <img src={props.image} alt="Nasa-galaxy" />
-            <p>Date: {props.date}</p>
-            <p>Description: {props.description}</p>
+            <p><strong>Date:</strong> {props.date}</p>
+            <p><strong>Description:</strong> {props.description}</p>
             <button>Like</button>
         </div>
      );
--- src/components/NasaPicturesGrid.js
+++ src/components/NasaPicturesGrid.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import NasaPicture from './NasaPicture';
+import './NasaPicturesGrid.scss';
 
 
 const NasaPicturesGrid = () => {
