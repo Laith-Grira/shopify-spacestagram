ff13e2e88d82426fa355ccc09430fe277e8f75d4
Laith-Grira <lgrira12@gmail.com>
2022-01-06 19:09:03 -0500

    more styles and maxing page responsive

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ import './App.scss';
 function App() {
 
   return (
-    <div className="App">
-      <h1>Starting project</h1>
-      <NasaPicturesGrid />
+    <div>
+      <header>
+        <img src="https://www.smartenergydecisions.com/upload/images/company_images/shopify.png" alt="shopify-logo" />
+        <h3>Shopify Summer 2022 Coding Challange</h3>
+      </header>
+      <div className="App mt-3">
+        <span id='main-page-title'>Make Commerce Better for Everyone</span>
+        <NasaPicturesGrid />
+      </div>
     </div>
   );
 }
--- src/components/NasaPicture.js
+++ src/components/NasaPicture.js
@@ -8,10 +8,10 @@ const NasaPicture = props => {
     return ( 
         <div className="element-modal">
             <h2 id="picture-title">{props.title}</h2>
-            <img src={props.image} alt="Nasa-galaxy" />
+            <img src={props.image} alt="Nasa-galaxy" className="mb-3" />
             <p><strong>Date:</strong> {props.date}</p>
             <p><strong>Description:</strong> {props.description}</p>
-            <button>Like</button>
+            <button type="button" class="btn btn-primary">Like</button>
         </div>
      );
 }
