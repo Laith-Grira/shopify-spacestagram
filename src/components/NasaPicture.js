bc4ca4ce0044f1c2af0ef1291b2ce22b6a4f52a2
Laith-Grira <lgrira12@gmail.com>
2022-01-14 19:38:25 -0500

    adding the card component to the picture modal

--- src/components/NasaPicture.js
+++ src/components/NasaPicture.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Card } from '@shopify/polaris';
 import DateModifier from "./utils/DateModifier";
 import './NasaPicture.scss';
 
@@ -13,14 +14,15 @@ const NasaPicture = props => {
 
     return ( 
         <div className="element-modal">
-            <h2 id="picture-title">{props.title}</h2>
-            <img src={props.image} alt="Nasa-galaxy" className="mb-3" />
-            <p><strong>Date:</strong> <DateModifier  date={props.date} /> </p>
-            <p><strong>Description:</strong> {props.description}</p>
-            <div className="like-toggle">
-                <button type="button" className="btn btn-primary" disabled={likeToggle} onClick={onToggleLikeButton}>Like</button>
-                <button type="button" className="btn btn-danger" disabled={!likeToggle} onClick={onToggleLikeButton}>Unlike</button>
-            </div>
+            <Card title={props.title} sectioned>
+                <img src={props.image} alt="Nasa-galaxy" className="mb-3" />
+                <p><strong>Date:</strong> <DateModifier  date={props.date} /> </p>
+                <p><strong>Description:</strong> {props.description}</p>
+                <div className="like-toggle mt-5">
+                    <button type="button" className="btn btn-primary" disabled={likeToggle} onClick={onToggleLikeButton}>Like</button>
+                    <button type="button" className="btn btn-danger" disabled={!likeToggle} onClick={onToggleLikeButton}>Unlike</button>
+                </div>
+            </Card>
         </div>
      );
 }
