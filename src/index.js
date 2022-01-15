b5ab7fea9b714a30393e3242695cf14d5381bfff
Laith-Grira <lgrira12@gmail.com>
2022-01-14 20:31:17 -0500

    fixing some pages style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { AppProvider } from "@shopify/polaris";
 import App from './App';
 import en from '@shopify/polaris/locales/en.json';
 import "@shopify/polaris/build/esm/styles.css";
-import './index.scss';
 
 ReactDOM.render(
   <AppProvider i18n={en} theme={{colorScheme: "light"}}>
